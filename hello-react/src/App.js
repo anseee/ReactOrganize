b7c1c33415fe3455d926c29f379bff676c5a7eac
anseee <parksungwon1113@gmail.com>
2021-12-25 17:39:59 +0900

    JSX 문법 정리중.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// Virtual Dom에서 컴포넌트를 감지해 낼때 효율적으로 비교하기 위해서 
+// 컴포넌트 내부는 하나의 DOM 트리로 이루어져야 한다.
+// <div> 사용 or Fragment 사용
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Hello React!</h1>
+      <h2>How are you?</h2>
+    </>
+  )
 }
-
 export default App;
