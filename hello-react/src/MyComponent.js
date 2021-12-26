4c792fb06a7e6984e2ae999f1e204f0bf9905985
anseee <parksungwon1113@gmail.com>
2021-12-26 14:20:08 +0900

    - 컴포넌트 정리중, react 기본 문법 정리 props, js arrow function

---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// 비구조화 할당 문법
+const MyComponent = ({ name, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name} 입니다.
+      <br />
+      children 값은 {children}
+      입니다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: '기본이름',
+};
+
+export default MyComponent;
