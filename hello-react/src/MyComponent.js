4831dee9d59456c10f5f7d40a14d2342a414b406
anseee <parksungwon1113@gmail.com>
2021-12-27 22:37:26 +0900

    PropTypes 정리

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // 비구조화 할당 문법
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name} 입니다.
       <br />
       children 값은 {children}
       입니다.
+      <br />
+      좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
 
 MyComponent.defaultProps = {
   name: '기본이름',
+};
+
+MyComponent.prototypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
