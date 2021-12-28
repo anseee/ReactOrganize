be40981249e5057cde0b62d4483cc732a0ee1813
anseee <parksungwon1113@gmail.com>
2021-12-28 17:08:10 +0900

    state callback 함수 구현, 간편 생성자 코드 구현

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   // 생성자
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
 
   render() {
-    const { number } = this.state; // state 조회
+    const { number, fixedNumber } = this.state; // state 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h1>바뀌지 않는 값: {fixedNumber}</h1>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 }); // setState를 사용하여 state에 새로운 값을 넣을 수 있다.
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('call setState');
+              }
+            );
           }}
         >
           +1
