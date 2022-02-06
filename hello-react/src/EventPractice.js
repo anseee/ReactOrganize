b86309f800eabcc1aa4e207c1ad3bc8c4a945b45
anseee <parksungwon1113@gmail.com>
2022-02-06 12:02:24 +0900

    [*] 4.2.3.2 Property Initializer Syntax

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,20 @@ class EventPractice extends Component {
   state = {
     message: '',
   };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    alert(this.state.message);
+    this.setState({
+      message: '',
+    });
+  };
+
   render() {
     return (
       <div>
@@ -13,20 +27,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({ message: e.target.value });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: '',
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
